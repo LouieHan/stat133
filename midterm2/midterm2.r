49089135d5ae0ac1fdd8288d1a662dea484c328d
LouieHan <yourfriendryanlee@berkeley.edu>
2015-04-02 15:32:03 -0700

    midterm2 partially done

--- midterm2/midterm2.r
+++ midterm2/midterm2.r
@@ -8,19 +8,34 @@
 # entries: 6, 12, 18, etc.
 
 #x <- <your code here>
+present <- yes
 
+x <- seq(from = 6, by = 6, length.out = 2000)
 
 # [1 pt]
 # Create [y], a logical vector of length 2000 
 # with y[i]=T if x[i] is divisible by 10, otherwise F
 
 # y <- <your code here>
+logi_func <- function(){
+x <- seq(from = 6, by = 6, length.out = 2000)
+logi <- NULL
+classy <- NULL
+for (i in 1:2000){
+  logi[i] <- is.integer(integer(x[i])/10)
+  i = i + 1
+}
+return(logi)
+}
+
+y <- logi_func()
 
 # [1 pt]
 # Create [w], a random permutation of the numeric values of a deck of cards
 # (i.e. just the numbers 1 through 13 each repeated 4 times)
 set.seed(2718)
 #w <- <your code here>
+
 
 
 # [1 pt]
@@ -30,6 +45,8 @@ set.seed(2718)
 set.seed(344)
 
 #m <- <your code here>
+exponential_entry <- rexp(100, 3)
+m <- matrix(exponential_entry, nrow = 10, ncol = 10, byrow = FALSE)
 
 
 # [1 pt]
@@ -37,6 +54,20 @@ set.seed(344)
 # Each vector of length 100 of Poisson (hint:rpois) random variables with mean 5
 set.seed(71)
 #l <- <your code here>
+
+pois_entry <- function(){
+  enter = NULL
+  i = 1
+  for (i in 1:12){
+    enter[i] <- rpois(100,5)
+    use_list[i] <- enter[i]
+    i = i + 1
+  }
+  return(list(enter))
+}
+
+l <- pois_entry
+
 
 
 # for the next two tasks you will use the data frame infants (size 1236x15)
@@ -46,11 +77,13 @@ load("KaiserBabies.rda")
 # [2 pt]
 # Create a table [t] of the education level ($ed) of all married ($marital) first time ($parity=1) mothers:
 #t <- <your code here>
-
+t <- table(infants$ed[infants$parity ==1], infants$parity[infants$parity ==1])
 
 # [2 pt]
 # Calculate [mw], the average birthweight ($bwt) of all babies whose were full term, i.e. gestation equal or more than 259 days.
 #mw <- <your code here>
+blank <- infants$bwt[infants$gestation>=259]
+mw <- mean(blank)
 
 
 # For the next few tasks you will use the data frame family (size 14x5)
@@ -60,24 +93,26 @@ load("family.rda")
 # [1 pt]
 # Create [f1] a subset of family with only women over age 50
 #f <- <your code here>
+f <- subset(subset(family, gender == "f", select = name:weight), age > 50, select = name:weight)
 
   
 # [1 pt]
 # Create [f2] a subset of family with only men 6 foot tall or more
 #fm <- <your code here>
 
+f2 <- subset(subset(family, gender == "m", select = name:weight), height >= 72, select = name:weight)
   
 # [1 pt]
 # Create [f3] a subset of family of people whose name starts with T
 #f3 <- <your code here>
-  
 
+f3 <- subset(family, name == "Tim"|name == "Tom", name:weight)
 
 # [1 pt]
 # Create [f4] a subset of family with just the youngest individual (so just one row)
 #f4 <- <your code here>
 
-
+f4 <- subset(family, age == min(age), name:weight)
 
 
 ## Plotting
